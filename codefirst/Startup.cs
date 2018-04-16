c4b74f33f3644310206bdadd603bb464283dcff3
gbahugun <gaganbahugun@hotmail.com>
2018-04-16 11:03:04 -0700

    Add project files.

---
+++ codefirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(codefirst.Startup))]
+namespace codefirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
